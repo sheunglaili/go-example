863ca300ec532d19e96d269fbe8e0b81dbd73403
sheunglaili <p313avee@gmail.com>
2020-03-16 16:20:32 +0800

    sync

---
+++ sync/counter.go
@@ -0,0 +1,22 @@
+package sync
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
+
+func (c Counter) Value() int {
+	return c.count
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
